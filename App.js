cf80fddc5f3f648b10ee2c6aad73e22a76210cca
Henrique Xavier <67128873+henxavier@users.noreply.github.com>
2021-04-13 10:16:03 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import TelaInicial from './componentes/Telas/TelaInicial'
+
+export default function App() {
+  return (
+    <TelaInicial />
+  );
+}
---
+++ gameData.js
@@ -0,0 +1,60 @@
+export default games = [
+    {
+        id: 1,
+        title: "Unforeseen Incidents",
+        teaser: "A classical style interactive mytery set in a beatifully",
+        description:
+            "Unforeseen incidents is a classical style interactive mystery set in beautifully",
+        rating: 4.5,
+        category: ["Action", "Indie"],
+        age: "12",
+        cover: require("./assets/unforeseen_1.jpg"),
+        screenshots: [
+            require("./assets/unforeseen_2.jpg"),
+            require("./assets/unforeseen_3.jpg"),
+            require("./essets/unforeseen_4.jpg"),
+            require("./essets/unforeseen_5.jpg"),
+            require("./essets/unforeseen_6.jpg"),
+        ],
+        backgroundColor: "#824571",
+    },
+    {
+        id: 2,
+        title: "Ori and the Will of the Wisps",
+        teaser: "Embark on a neew jorney in a vast, exotic world where you'll",
+        description:
+            "Play the critically acclaimed masterpice",
+        rating: 3.3,
+        category: ["Strategy", "Indie", "Action"],
+        age: "11+",
+        cover: require("./assets/ori_1.jpg"),
+        screenshots: [
+            require("./essets/ori_2.jpg"),
+            require("./essets/ori_3.jpg"),
+            require("./essets/ori_4.jpg"),
+            require("./essets/ori_5.jpg"),
+            require("./essets/ori_6.jpg"),
+        ],
+        backgroundColor: "#456382",
+    },
+    {
+        id: 3,
+        title: "Unravel",
+        teaser: "Yarny a character made from a single thread of yarn",
+        description:
+            "Unreal is a physical based puzzle platform",
+        rating: 2.4,
+        category: ["Casual", "Indie", "Adventure"],
+        age: "13+",
+        cover: require("./assets/unravel_1.jpg"),
+        screenshots: [
+            require("./essets/unravel_2.jpg"),
+            require("./essets/unravel_3.jpg"),
+            require("./essets/unravel_4.jpg"),
+            require("./essets/unravel_5.jpg"),
+            require("./essets/unravel_6.jpg"),
+        ],
+        backgroundColor: "#456382",
+    },
+
+]
